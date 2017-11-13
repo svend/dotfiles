9d2a3d829f497977360ef0c164adfdc24c55c51f
Svend Sorensen <svend@svends.net>
2017-11-13 11:44:33 -0800

    Limit print-length

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,4 @@
-{:user {:plugins [[lein-kibit "0.1.5"]
+{:user {:repl-options {:init (set! *print-length* 50)}
+        :plugins [[lein-kibit "0.1.5"]
                   [lein-try "0.4.3"]
                   [venantius/ultra "0.5.1"]]}}
