e857d7aa6f266e9bdf746797f536c95a8925b200
Svend Sorensen <svend@svends.net>
2018-02-06 10:32:09 -0800

    Fix notmuch build on darwin

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -107,6 +107,36 @@
       };
     });
 
+    # 2018-02-02 Fix "couldn't find libtalloc in nix store"
+    notmuch = pkgs.notmuch.overrideAttrs (oldAttrs: rec {
+      preFixup = lib.optionalString stdenv.isDarwin ''
+        set -e
+
+        die() {
+          >&2 echo "$@"
+          exit 1
+        }
+
+        prg="$out/bin/notmuch"
+        lib="$(find "$out/lib" -name 'libnotmuch.?.dylib')"
+
+        [[ -s "$prg" ]] || die "couldn't find notmuch binary"
+        [[ -s "$lib" ]] || die "couldn't find libnotmuch"
+
+        badname="$(otool -L "$prg" | awk '$1 ~ /libtalloc/ { print $1 }')"
+        goodname="$(find "${talloc}/lib" -name 'libtalloc.*.*.*.dylib')"
+
+        [[ -n "$badname" ]]  || die "couldn't find libtalloc reference in binary"
+        [[ -n "$goodname" ]] || die "couldn't find libtalloc in nix store"
+
+        echo "fixing libtalloc link in $lib"
+        install_name_tool -change "$badname" "$goodname" "$lib"
+
+        echo "fixing libtalloc link in $prg"
+        install_name_tool -change "$badname" "$goodname" "$prg"
+      '';
+    });
+
     xonsh = pkgs.xonsh.overrideAttrs (oldAttrs: rec {
       # Check fails on macOS
       doInstallCheck = !stdenv.isDarwin;
