13c4828106bd9750ce8731c9effa5d1a616b9a0c
Svend Sorensen <svend@svends.net>
2018-02-06 12:22:56 -0800

    Move notmuch

--- .config/nixpkgs/overlays/userEnv.nix
+++ .config/nixpkgs/overlays/userEnv.nix
@@ -98,37 +98,6 @@ with super.pkgs;
   ]) ++
   [emacs25PackagesNg.pdf-tools]);
 
-    # 2018-02-02 Fix "couldn't find libtalloc in nix store"
-    # https://github.com/NixOS/nixpkgs/pull/34577
-    my_notmuch = pkgs.notmuch.overrideAttrs (oldAttrs: rec {
-      preFixup = lib.optionalString stdenv.isDarwin ''
-        set -e
-
-        die() {
-          >&2 echo "$@"
-          exit 1
-        }
-
-        prg="$out/bin/notmuch"
-        lib="$(find "$out/lib" -name 'libnotmuch.?.dylib')"
-
-        [[ -s "$prg" ]] || die "couldn't find notmuch binary"
-        [[ -s "$lib" ]] || die "couldn't find libnotmuch"
-
-        badname="$(otool -L "$prg" | awk '$1 ~ /libtalloc/ { print $1 }')"
-        goodname="$(find "${talloc}/lib" -name 'libtalloc.*.*.*.dylib')"
-
-        [[ -n "$badname" ]]  || die "couldn't find libtalloc reference in binary"
-        [[ -n "$goodname" ]] || die "couldn't find libtalloc in nix store"
-
-        echo "fixing libtalloc link in $lib"
-        install_name_tool -change "$badname" "$goodname" "$lib"
-
-        echo "fixing libtalloc link in $prg"
-        install_name_tool -change "$badname" "$goodname" "$prg"
-      '';
-    });
-
     userEnv = pkgs.buildEnv {
       name = "userEnv";
       paths = [
@@ -171,7 +140,7 @@ with super.pkgs;
         netcat-gnu
         nim
         nix-prefetch-scripts
-        my_notmuch
+        notmuch
         openssl
         openssh
         # pandoc # FTB 2017-09-22
