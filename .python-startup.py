67a10a66ec36292243931a77e2da2c7691743dcb
Svend Sorensen <svend@svends.net>
2018-03-02 21:55:59 -0800

    Set dynamic width

--- .python-startup.py
+++ .python-startup.py
@@ -1,4 +1,12 @@
+import os
 import pprint
 import sys
 
-sys.displayhook = lambda x: pprint.pprint(x, compact=True)
+
+def mypprint(x):
+    columns = os.get_terminal_size().columns
+    pprint.pprint(x, width=columns or 80, compact=True)
+
+
+sys.displayhook = mypprint
+# sys.displayhook = lambda x: pprint.pprint(x, compact=True)
