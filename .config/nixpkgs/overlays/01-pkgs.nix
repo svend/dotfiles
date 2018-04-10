c534f5a347536f50718440b9792386884bca600f
Svend Sorensen <svend@svends.net>
2018-04-10 15:12:45 -0700

    Install black

--- .config/nixpkgs/overlays/01-pkgs.nix
+++ .config/nixpkgs/overlays/01-pkgs.nix
@@ -1,5 +1,6 @@
 self: super:
 {
+  black = super.callPackage ../pkgs/black { };
   cargo-script = super.callPackage ../pkgs/cargo-script { };
   errcheck = super.callPackage ../pkgs/errcheck { };
 }
