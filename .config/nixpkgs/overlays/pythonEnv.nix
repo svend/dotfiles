2e48fec06d23116314cf85350d34c5513076d79d
Svend Sorensen <svend@svends.net>
2018-08-25 13:14:09 -0700

    Python 3.7

--- .config/nixpkgs/overlays/pythonEnv.nix
+++ .config/nixpkgs/overlays/pythonEnv.nix
@@ -10,19 +10,21 @@ self: super:
           virtualenv
         ];
       })
-        (with python3Packages; python.buildEnv.override {
+
+      (with python37Packages; python.buildEnv.override {
         extraLibs = [
           black
           flake8
           isort
           pip
-          pylint
+          # pylint # astroid-2.0.1 FTB on Python 3.7
           pyyaml
-          requests
+          # requests # hypothesis-3.66.2 FTB on Python 3.7
         ];
       })
-        xonsh
-        # pipenv # Error while finding module specification for 'pipenv.pew' (ModuleNotFoundError: No module named 'pipenv')
-      ];
+
+      xonsh
+      # pipenv # Error while finding module specification for 'pipenv.pew' (ModuleNotFoundError: No module named 'pipenv')
+    ];
   };
 }
