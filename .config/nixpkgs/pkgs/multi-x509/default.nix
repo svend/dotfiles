0ff3cbb7bf743f7a2493019a583ef1dcf1fd7be6
Svend Sorensen <svend@svends.net>
2018-09-09 11:11:42 -0700

    Update

--- .config/nixpkgs/pkgs/multi-x509/default.nix
+++ .config/nixpkgs/pkgs/multi-x509/default.nix
@@ -3,18 +3,18 @@
 with rustPlatform;
 
 buildRustPackage rec {
-  name = "cargo-script-${version}";
-  version = "2018-09-09";
+  name = "multi-x509-${version}";
+  version = "0.1.0";
 
   src = fetchFromGitHub {
     owner = "svend";
     repo = "multi-x509";
     # rev = "${version}";
     rev = "6798d5f7b7c5679743362c86ad4676b7bd906ba4";
-    sha256 = "18fljjr49lpi1lyxwfx0bsp9fhz5ii63w8hsskjqahgvdqay3bdf";
+    sha256 = "1h70vyxx843bqyx447jkd08prpq1xdjxn2yy0lnfj8dkcwm6x4jf";
   };
 
-  cargoSha256 = "1cgqk1dj8ln1x0ll63h83alrazhic20bqjzrn0kmphf136d9z8fm";
+  cargoSha256 = "1ii9sqfm6ipw0nxpc0pcncrg6xylglp6srdznjwmqv233k60ra5h";
   doCheck = false;
   meta = with stdenv.lib; {
     description = "Run a command on each x509 certificate";
