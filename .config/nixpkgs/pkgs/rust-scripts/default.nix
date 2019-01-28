c131bc1a82606711b6fa095e606c06a68969bb36
Svend Sorensen <svend@svends.net>
2019-01-28 15:05:18 -0800

    Update

--- .config/nixpkgs/pkgs/rust-scripts/default.nix
+++ .config/nixpkgs/pkgs/rust-scripts/default.nix
@@ -10,8 +10,8 @@ buildRustPackage rec {
     owner = "svend";
     repo = "rust-scripts";
     # rev = "${version}";
-    rev = "dd415524aad6dd92d72dd27823d6f8ac2d6e4850";
-    sha256 = "08y0yvv3bkywkc0vbh787lyfhzd335h55wyxfjfch3mxhp4mkk5r";
+    rev = "4621b59a98c908e00d6de078b6f5e844f2b8f094";
+    sha256 = "09hpz44n5nxr6zqa1aadp40509z8d8y9262hck99a98az73s1nvx";
   };
 
   # buildRustPackage requires a cargoSha256 attribute which is computed over all
