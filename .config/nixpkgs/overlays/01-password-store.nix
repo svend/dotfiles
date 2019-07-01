08eb7a03a7e88d416652252cf9f5588dda299a6f
Svend Sorensen <svend@svends.net>
2019-07-01 15:26:47 -0700

    Remove unnecessary overlay

--- .config/nixpkgs/overlays/01-password-store.nix
+++
@@ -1,8 +0,0 @@
-self: super:
-{
-  pass = with super; pass.overrideAttrs (old: rec {
-    # Check fails macOS 2019-03-26
-    # not ok 1 - Test "generate" command
-    doInstallCheck = !stdenv.isDarwin;
-  });
-}
