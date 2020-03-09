aa0025f0cc37d7d631a14f5eeb0b21686406dce2
Svend Sorensen <svend@svends.net>
2020-03-09 13:15:55 -0700

    Update rust-analyzer

--- .config/nixpkgs/pkgs/rust-analyzer/default.nix
+++ .config/nixpkgs/pkgs/rust-analyzer/default.nix
@@ -4,20 +4,20 @@ with rustPlatform;
 
 buildRustPackage rec {
   name = "rust-analyzer-${version}";
-  version = "2020-03-02";
+  version = "2020-03-09";
 
   src = fetchFromGitHub {
     owner = "rust-analyzer";
     repo = "rust-analyzer";
     rev = "${version}";
-    sha256 = "1xpzj1ilf0q98ilbmq2gw71aa07ypl5qj7s6irjisnaqghgm7kqf";
+    sha256 = "1m97sinfyg43p3crhbjrsgf64bn5pyj7m96ikvznps80w8dnsv5n";
   };
 
   # buildRustPackage requires a cargoSha256 attribute which is computed over all
   # crate sources of this package. Currently it is obtained by inserting a fake
   # checksum into the expression and building the package once. The correct
   # checksum can be then take from the failed build.
-  cargoSha256 = "0rq4ig5qnhcxmp1gkishrl62xc7pb16c771qn4hcqcqv1840r6z6";
+  cargoSha256 = "1n6rxsi1726x0c1y3k335nsw2y0wcml0ls3px3nxmlif9hdxqchq";
 
   buildInputs = stdenv.lib.optional stdenv.isDarwin [CoreServices Security];
 
