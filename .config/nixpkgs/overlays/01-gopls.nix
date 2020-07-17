23ca7a8f8dcfbd5cc7f8401c57143c015a2fa418
Svend Sorensen <svend@svends.net>
2020-07-17 11:18:44 -0700

    Update gopls to 0.4.3

--- .config/nixpkgs/overlays/01-gopls.nix
+++ .config/nixpkgs/overlays/01-gopls.nix
@@ -1,15 +1,15 @@
 self: super:
 {
-  # TODO: overriding vendorSha256 has no effect
   # gopls = with super; gopls.overrideAttrs (old: rec {
-  #   version = "0.4.2";
+  #   version = "0.4.3";
 
   #   src = fetchgit {
   #     rev = "gopls/v${version}";
   #     url = "https://go.googlesource.com/tools";
-  #     sha256 = "1i9q9842d7ndzqcpg934brhm27lrfjjdlf91ly8ipwpsms574jlx";
+  #     sha256 = "1r670c7p63l0fhx671r3mb1jgvvfv1382079fv59z07j5j5hizbc";
   #   };
 
-  #   # vendorSha256 = "0000000000000000000000000000000000000000000000000000";
+  #   # TODO: overriding vendorSha256 has no effect
+  #   vendorSha256 = "1xdvkdkvk7a32jspzjcgxkfdn78d2zm53wxmc9c4sqysxsgy6lbw";
   # });
 }
--- .config/nixpkgs/pkgs/gopls/default.nix
+++ .config/nixpkgs/pkgs/gopls/default.nix
@@ -2,21 +2,21 @@
 
 buildGoModule rec {
   pname = "gopls";
-  version = "0.4.2";
+  version = "0.4.3";
 
   src = fetchgit {
     rev = "gopls/v${version}";
     url = "https://go.googlesource.com/tools";
-    sha256 = "1i9q9842d7ndzqcpg934brhm27lrfjjdlf91ly8ipwpsms574jlx";
+    sha256 = "1r670c7p63l0fhx671r3mb1jgvvfv1382079fv59z07j5j5hizbc";
   };
 
   modRoot = "gopls";
-  vendorSha256 = "0c2h03kqyh2jjp9957kmpybgw17apc4np3za9hzg8j0d8kkli5mm";
+  vendorSha256 = "1xdvkdkvk7a32jspzjcgxkfdn78d2zm53wxmc9c4sqysxsgy6lbw";
 
   meta = with stdenv.lib; {
     description = "Official language server for the Go language";
     homepage = "https://github.com/golang/tools/tree/master/gopls";
     license = licenses.bsd3;
-    maintainers = with maintainers; [ mic92 ];
+    maintainers = with maintainers; [ mic92 zimbatm ];
   };
 }
