a5380c836d99cc2c8a2b366f882ccce9bca84315
Svend Sorensen <svend@svends.net>
2020-08-06 08:45:16 -0700

    Re-enable notmuch tests

--- .config/nixpkgs/overlays/01-notmuch.nix
+++ .config/nixpkgs/overlays/01-notmuch.nix
@@ -1,8 +1,9 @@
 self: super:
 {
-  notmuch = with super; notmuch.overrideAttrs (old: rec {
-    # Tests fail with:
-    # FATAL: /build/notmuch-0.29.3/test/T310-emacs.sh: interrupted by signal 15
-    doCheck = false;
-  });
+  # TODO: Remove this file
+  # notmuch = with super; notmuch.overrideAttrs (old: rec {
+  #   # Tests fail with:
+  #   # FATAL: /build/notmuch-0.29.3/test/T310-emacs.sh: interrupted by signal 15
+  #   doCheck = false;
+  # });
 }
