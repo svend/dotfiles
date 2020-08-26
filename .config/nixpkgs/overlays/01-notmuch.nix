82b2100cea4d190b5833901aea5effbf20e3f838
Svend Sorensen <svend@svends.net>
2020-08-26 09:43:41 -0700

    Remove notmuch overlay

--- .config/nixpkgs/overlays/01-notmuch.nix
+++
@@ -1,8 +0,0 @@
-self: super:
-{
-  notmuch = with super; notmuch.overrideAttrs (old: rec {
-    # Tests fail with:
-    # FATAL: /build/notmuch-0.29.3/test/T310-emacs.sh: interrupted by signal 15
-    doCheck = false;
-  });
-}
