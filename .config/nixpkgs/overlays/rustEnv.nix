7d1f7436e8d67822668b7bc4cd902edd2e7be677
Svend Sorensen <svend@svends.net>
2020-09-21 11:46:38 -0700

    Update rustEnv

--- .config/nixpkgs/overlays/rustEnv.nix
+++ .config/nixpkgs/overlays/rustEnv.nix
@@ -3,19 +3,21 @@ self: super:
   rustEnv = with self; super.buildEnv {
     name = "rustEnv";
     paths = [
-      # cargo
-      # rustc
-      # rustfmt
-
+      # Tools required by rustc
       # gcc
       # gnumake
       # binutils-unwrapped
 
       # rustup
 
-      cargo-edit
-      latest.rustChannels.stable.rust
-      rust-analyzer
+      cargo-edit # cargo add/rm/upgrade (https://github.com/killercup/cargo-edit)
+
+      # latest.rustChannels.stable.rust
+
+      # cargo
+      # rustc
+      # rustfmt
+      rust-analyzer # collision with rust from nixpkgs-mozilla
     ];
   };
 }
