1f3143652579893b7a252c1661f1de5f87f2f03b
Svend Sorensen <svend@svends.net>
2020-10-03 15:52:06 -0700

    Enable nix flakes

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -2,4 +2,7 @@
   # Allow installation on darwin platform
   allowUnsupportedSystem = true;
   allowUnfree = true;
+
+  # Enable Nix Flakes: https://nixos.wiki/wiki/Flakes
+  experimental-features = nix-command flakes;
 }
