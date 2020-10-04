7fe6e4ad47b7f697033e266354ed037e87320648
Svend Sorensen <svend@svends.net>
2020-10-04 10:19:29 -0700

    Move nix flake config to the correct file

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -2,7 +2,4 @@
   # Allow installation on darwin platform
   allowUnsupportedSystem = true;
   allowUnfree = true;
-
-  # Enable Nix Flakes: https://nixos.wiki/wiki/Flakes
-  # experimental-features = nix-command flakes;
 }
