1d6cdfeb7256a42521c43a69374e8b4ab7dd18af
Svend Sorensen <svend@svends.net>
2020-10-23 22:30:25 -0700

    Remove

--- .lein/profiles.clj
+++
@@ -1,5 +0,0 @@
-{:user {:repl-options {:init (set! *print-length* 50)}
-        :plugins [[lein-kibit "0.1.5"]
-                  [lein-try "0.4.3"]
-                  ;; [venantius/ultra "0.5.1"] ;; https://github.com/clojure-emacs/cider/issues/2114
-                  ]}}
